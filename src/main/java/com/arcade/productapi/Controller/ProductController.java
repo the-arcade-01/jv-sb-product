114a185b9db1a04c15521dd566b693b05cfd929e
the-arcade-01 <aashisharcade@gmail.com>
2022-07-19 16:11:06 +0530

    product controller done

--- src/main/java/com/arcade/productapi/Controller/ProductController.java
+++ src/main/java/com/arcade/productapi/Controller/ProductController.java
@@ -1,10 +1,64 @@
 package com.arcade.productapi.Controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import com.arcade.productapi.Model.ProductModel;
+import com.arcade.productapi.Service.ProductService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@RequestMapping(path="/api/products")
+@RequestMapping(path = "/api/products")
 public class ProductController {
-    
+    private final ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductModel>> getAllProducts() {
+        List<ProductModel> products = productService.getAllProducts();
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getOneProduct(@PathVariable Long id) {
+        Optional<ProductModel> product = productService.getOneProduct(id);
+        return new ResponseEntity<>(product, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductModel> createProduct(@RequestBody ProductModel product,
+            @RequestParam("file") MultipartFile file) throws Exception {
+        ProductModel newProduct = productService.createProduct(product, file);
+        return new ResponseEntity<>(newProduct, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/product/{id}")
+    public ResponseEntity<?> updateProduct(@RequestBody ProductModel product, @RequestParam("file") MultipartFile file,
+            @PathVariable Long id) throws Exception {
+        Optional<Object> updatedProduct = productService.updateProduct(product, file, id);
+        return new ResponseEntity<>(updatedProduct, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
